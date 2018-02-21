921cd729b86f53800078163fc32ab70db6eb95af
Ananya Kokala <33649429+Ananyakokala@users.noreply.github.com>
2018-02-21 23:09:07 +0530

    Add files via upload

---
+++ php/popup.php
@@ -0,0 +1,47 @@
+<?php
+
+$to = "nagabhushan.pai@gmail.com";  
+$subject="Client Response";
+
+$name=$_POST['name'];
+$mobile=$_POST['mobile'];
+$you_are=$_POST['position'];//radio value
+
+$study=array();
+$course=$_POST['course'];//check box value
+$i=0;
+foreach ($course as $c)
+{ 
+  $study[$i]=$c;
+  $i=$i+1;
+}
+
+$atttempt=$_POST['due'];//radio value
+
+$article=$_POST['article'];//radio button retrial 
+$initial=$_POST['attempt'];//radio button retrial 
+
+$message="Name :"."$name"."\n".
+         "Contact Number: "."$mobile"."\n".
+		 "Personalization:"."$you_are"."\n".
+		 "Courses that are looking for:"."$study"."\n".
+		 "Next attempt Due date"."$attempt"."\n".
+		 "Article-ship"."article"."\n".
+		 "Number of attempts:"."$initial";
+		 
+$headers="MIME-VERSION: 1.0" . "\r\n";
+$headers .="Content-type=text/html;charset=utf-8" . "\r\n";
+$headers .="From : <$name> \r\n";
+
+if(mail($to,$subject,$message))
+{
+	echo "<script> 
+	         alert('You submitted successfully! Thank you for your response'); 
+	      </script>";
+}
+else 
+{
+    echo "<script>alert('Sorry!! Could not send your mail!!Please retry');</script>";
+    
+	}
+?>
