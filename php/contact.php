0a085c5255430aa94e2749e9888f04c12612d528
Ananya Kokala <33649429+Ananyakokala@users.noreply.github.com>
2018-02-21 20:36:54 +0530

    Update contact.php

--- php/contact.php
+++ php/contact.php
@@ -1,17 +1,3 @@
-<?php
-
-$to = "nagabhushan.pai@gmail.com";    
-$message = $_POST['message'];
-$subject=$_POST['sendersubject'];
-$personemail=$_POST['senderemail'];
-$person=$_POST['sendername'];
-$phone=$_POST['contact'];
-$headers="MIME-VERSION: 1.0" . "\r\n";
-$headers .="Content-type=text/html; charset=UTF-8" . "\r\n";
-$headers="From : <$personemail> \r\n";
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -113,24 +99,8 @@ $headers="From : <$personemail> \r\n";
 								<textarea class="form-control" name="message" placeholder="Write you message here..." style="height:100px;"></textarea>
 							</div>
 							<button type="submit" name="submit" class="btn btn-two">Send message</button><p><br/></p>
-							<?php
-							if(isset($_POST['submit']))
-							{
-	                           				if(mail($to,$subject,$message, $headers))
-								{
-								  echo "<script> 
-	         						          alert('Your message sent successfully! Thank you $person, for your response'); 
-	      							       </script>";
-								}
-								else 
-								{
-   								   echo "<script>
-								   alert('Sorry!! Could not send your mail!!Please retry');
-								   </script>";
-    
-							        }
-							}
-							?>
+							
+							
 						</form>
 					</div>
 					<div class="col-md-4">
